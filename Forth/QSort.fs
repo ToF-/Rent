f9013cb5821828e3c5caae0ea1cb80febb0c33cf
ToF- <cthibaut@octo.com>
2016-12-20 16:46:52 +0100

    forth sort

--- Forth/QSort.fs
+++ Forth/QSort.fs
@@ -3,7 +3,29 @@
 : CELL- CELL - ;
 
 : LT-PIVOT ( p a -- p a' first address of value >= p starting from p+1 )
-    BEGIN CELL+ 2DUP @ > WHILE REPEAT ; 
+    BEGIN 2DUP @ > WHILE CELL+ REPEAT ; 
 
 : GT-PIVOT ( p a -- p a' first address of value <= p starting from p-1 )
-    BEGIN CELL- 2DUP @ < WHILE REPEAT ; 
+    BEGIN 2DUP @ < WHILE CELL- REPEAT ; 
+
+: EXCHANGE ( a b -- swap address a and b content )
+    2DUP @ SWAP @ ROT ! SWAP ! ;
+
+: PARTITION ( h l -- h  partitions the array, leaving new pivot )
+    DUP @   \ h l p
+    BEGIN
+        SWAP LT-PIVOT SWAP \ h l p  
+        ROT GT-PIVOT ROT   \ p h l
+        2DUP > IF          \ p h l
+            2DUP EXCHANGE  \ p h l
+            CELL+ SWAP CELL- SWAP 
+            ROT TRUE
+        ELSE ROT 2DROP FALSE THEN
+    WHILE 
+    REPEAT ;
+
+: QSORT ( h l -- sort the array )
+    
+: SORT ( a l -- sort the array a of size l )
+    CELLS OVER + SWAP QSORT
+;
--- Forth/QSortTest.fs
+++ Forth/QSortTest.fs
@@ -8,12 +8,23 @@ T{
     4807 ,   42 ,  512 , 4096 , 1000 , 256 , 
 
     ." LT-PIVOT seek address of 1st item >= pivot" CR
-        4807 A CELL- LT-PIVOT NIP   A ?S  
-        512  A       LT-PIVOT NIP   A 2 CELLS + ?S
+        4807 A       LT-PIVOT NIP   A ?S  
+        512  A CELL+ LT-PIVOT NIP   A 2 CELLS + ?S
 
     ." GT-PIVOT seek address of 1st item <= pivot" CR
-        4807 A 6 CELLS + GT-PIVOT NIP   A 5 CELLS + ?S  
-        42   A 6 CELLS + GT-PIVOT NIP   A 1 CELLS + ?S  
+        4807 A 5 CELLS + GT-PIVOT NIP   A 5 CELLS + ?S  
+        42   A 5 CELLS + GT-PIVOT NIP   A 1 CELLS + ?S  
 
+    ." EXCHANGE swaps the content of two addresses" CR
+        A A 2 CELLS + EXCHANGE A @ 512 ?S A 2 CELLS + @ 4807 ?S  
+        A A 2 CELLS + EXCHANGE
+
+    ." PARTITION separate items < and >= to pivot, leaving next pivot" CR
+        A 5 CELLS + A PARTITION   A 4 CELLS + ?S
+        A 5 CELLS + @ 4807 ?S  A @ 256 ?S
+
+    ." SORT quick sorts an array" CR
+        A 5 SORT
+        A DUP @ 42 ?S CELL+ DUP @ 256 ?S CELL+ DUP @ 512 ?S CELL+ DUP @ 1000 ?S CELL+ DUP @ 4096 ?S CELL+ @ 4807 ?S
 }T
 BYE
