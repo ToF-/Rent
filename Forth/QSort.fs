dab5a56833f5455535104e70a9978905a640d7aa
ToF- <cthibaut@octo.com>
2016-12-20 15:16:50 +0100

    forth sort restart

--- Forth/QSort.fs
+++ Forth/QSort.fs
@@ -1,29 +1,9 @@
 \ QSort.fs
 
-: .SK .S KEY DROP ;
+: CELL- CELL - ;
 
-: CELL- ( addr -- addr )
-    CELL - ;
+: LT-PIVOT ( p a -- p a' first address of value >= p starting from p+1 )
+    BEGIN CELL+ 2DUP @ > WHILE REPEAT ; 
 
-: LT-PIVOT ( p lo -- p lo  seek for 1st value >= p in addr,addr+1,addr+2.. )
-    BEGIN CELL+ 2DUP @ > WHILE REPEAT ;
-     
-: GT-PIVOT ( p hi -- p hi   seek for 1st value <= p in addr,addr-1,addr-2.. )
-    BEGIN CELL- 2DUP @ < WHILE REPEAT ;
-
-: EXCHANGE ( a b -- exchange the values in a and b )
-    2DUP @ >R @ SWAP ! R> SWAP ! ;
-
-: PARTITION ( j i -- adp separates items lower and greater than lo+0, leaving new pivot )
-    DUP @                     \ j i p
-    ROT CELL+ ROT CELL-       \ p j=j+1 i=i-1 
-    BEGIN
-        ROT SWAP  LT-PIVOT    \ j p i
-        -ROT SWAP GT-PIVOT    \ i p j 
-        ROT 2DUP              \ p j i j i
-        >= IF                 \ p j i
-            2DUP EXCHANGE TRUE \ p j i t
-         ELSE FALSE THEN       \ p j i f
-    WHILE                      \ p j i
-    REPEAT
-    ROT 2DROP ;                 \ j
+: GT-PIVOT ( p a -- p a' first address of value <= p starting from p-1 )
+    BEGIN CELL- 2DUP @ < WHILE REPEAT ; 
--- Forth/QSortTest.fs
+++ Forth/QSortTest.fs
@@ -3,38 +3,17 @@
 REQUIRE ffl/tst.fs
 REQUIRE QSort.fs
 
-
-: .. ( addr n -- prints n values from addr,addr+1,addr+2.. )
-    0 DO DUP @ . CELL+ LOOP DROP ;
-
-: [] ( addr n -- addr   address of item n in array addr )
-    CELLS + ;
 T{
-    CREATE MYARRAY
+    CREATE A
     4807 ,   42 ,  512 , 4096 , 1000 , 256 , 
 
     ." LT-PIVOT seek address of 1st item >= pivot" CR
-        4096 MYARRAY 0 [] CELL-  LT-PIVOT MYARRAY 0 [] ?S 4096 ?S
-        4096 MYARRAY 1 [] CELL-  LT-PIVOT MYARRAY 3 [] ?S 4096 ?S
-    
+        4807 A CELL- LT-PIVOT NIP   A ?S  
+        512  A       LT-PIVOT NIP   A 2 CELLS + ?S
+
     ." GT-PIVOT seek address of 1st item <= pivot" CR
-        256  MYARRAY 5 [] CELL+  GT-PIVOT MYARRAY 5 [] ?S  256 ?S
-        4096 MYARRAY 4 [] CELL+  GT-PIVOT MYARRAY 4 [] ?S 4096 ?S
-        42   MYARRAY 5 [] CELL+  GT-PIVOT MYARRAY 1 [] ?S   42 ?S
+        4807 A 6 CELLS + GT-PIVOT NIP   A 5 CELLS + ?S  
+        42   A 6 CELLS + GT-PIVOT NIP   A 1 CELLS + ?S  
 
-    ." EXCHANGE exchanges two items" CR
-        MYARRAY 5 [] MYARRAY 0 [] EXCHANGE
-        MYARRAY 5 [] @ 4807 ?S
-        MYARRAY 0 [] @ 256  ?S
-        MYARRAY 5 [] MYARRAY 0 [] EXCHANGE
-
-    ." PARTITION separates items lower and greater than a pivot, returning address of next pivot" CR
-        MYARRAY 5 [] MYARRAY 0 [] PARTITION MYARRAY 4 [] ?S
-        MYARRAY 6 .. CR
-        MYARRAY 4 [] MYARRAY 0 [] PARTITION MYARRAY 0 [] ?S
-        MYARRAY 6 .. CR
-        MYARRAY 5 [] MYARRAY 4 [] PARTITION MYARRAY 0 [] ?S
-        MYARRAY 6 .. CR
 }T
 BYE
-
