36a984ef53fbff3b3cf71333787885cc30c66378
ToF <cthibaut@octo.com>
2017-07-08 12:54:58 +0200

    a single order should result in that order price -- triangulate

--- C/rent.c
+++ C/rent.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <assert.h>
+#define MAXLINE 4096
+
+char Line[MAXLINE];
+
+char *get_line(char *line) {
+    fgets(line, MAXLINE, stdin);
+    return line;
+}
+
+int get_int(char *line) {
+    int result;
+    sscanf(get_line(line), "%d", &result);
+    return result;
+}
 
 int main() {
-    printf("100\n");
+    int max_cases, max_orders;
+    int start_time, duration, price;
+    max_cases = get_int(Line);
+    for(int i=0; i<max_cases; i++) {
+        get_line(Line);
+        get_line(Line);
+        sscanf(Line, "%d %d %d", &start_time, &duration, &price); 
+        printf("%d\n", price);
+    }
     return 0;
 }
 
