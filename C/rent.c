20cfe3b4bb4c5430d07ce397fd68e7fbcb75d543
ToF <cthibaut@octo.com>
2017-07-08 13:59:15 +0200

    given ordered orders in a small range on distinct times, profit should be optimal -- refactoring

--- C/rent.c
+++ C/rent.c
@@ -6,7 +6,7 @@
 struct order{
     int start_time;
     int duration;
-    int price;
+    int value;
 } Orders[MAXORDER];
 
 char Line[MAXLINE];
@@ -26,36 +26,47 @@ int max(int a, int b) {
     return a > b ? a : b;
 }
 
-int main() {
+int next_compatible(int j, int max_orders) {
+    for ( int k=j+1; k<=max_orders; k++) {
+        if (Orders[k].start_time >= Orders[j].start_time + Orders[j].duration) 
+            return k;
+    }
+    return max_orders;
+}
+
+int calc_value(int max_orders) {
+    for(int j=max_orders-2; j>=0; j--) {
+        int k = next_compatible(j,max_orders);
+        Orders[j].value = max(Orders[j+1].value, Orders[j].value + Orders[k].value);
+    } 
+    return Orders[0].value;
+}
+
+int get_orders() {
+    int max_orders = get_int(Line);
+    for(int j=0; j<max_orders; j++) {
+        get_line(Line);
+        sscanf(Line, "%d %d %d", 
+                &Orders[j].start_time, 
+                &Orders[j].duration, 
+                &Orders[j].value); 
+    }
+    return max_orders;
+}
+
+void initialize() {
     for (int i=0; i<MAXORDER; i++) {
         Orders[i].start_time = 10000000;
         Orders[i].duration   = 0;
-        Orders[i].price      = 0;
+        Orders[i].value      = 0;
     }
-    int max_cases, max_orders;
-    int start_time, duration, price;
-    int value;
-    max_cases = get_int(Line);
+}
+int main() {
+    int max_cases = get_int(Line);
     for(int i=0; i<max_cases; i++) {
-        max_orders = get_int(Line);
-        value = 0;
-        for(int j=0; j<max_orders; j++) {
-            get_line(Line);
-            sscanf(Line, "%d %d %d", 
-                    &Orders[j].start_time, 
-                    &Orders[j].duration, 
-                    &Orders[j].price); 
-        }
-        max_orders++;
-        for(int j=max_orders-2; j>=0; j--) {
-            int k;
-            for ( k=j+1; k<=max_orders; k++) {
-                if (Orders[k].start_time >= Orders[j].start_time + Orders[j].duration) 
-                    break;
-            }
-            Orders[j].price = max(Orders[j+1].price, Orders[j].price + Orders[k].price);
-        } 
-        printf("%d\n", Orders[0].price);
+        initialize();
+        int max_orders = get_orders();
+        printf("%d\n", calc_value(max_orders+1));
     }
     return 0;
 }
