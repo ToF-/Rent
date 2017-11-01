470c60fc00fcdf778fdb5e48afa3f2364f9aefa3
ToF <cthibaut@octo.com>
2017-11-01 13:02:28 +0100

    an empty plan is worth 0

--- Rust/rent/src/lib.rs
+++ Rust/rent/src/lib.rs
@@ -7,8 +7,12 @@ struct Order {
 
 impl Order {
     fn compatible(&self, other:Order) -> bool {
-        true
+        self.start_time >= other.start_time + other.duration
     }
+}
+
+fn value(orders: Vec<Order>) -> i32 {
+    0
 }
 
 #[cfg(test)]
@@ -20,5 +24,22 @@ mod tests {
         let a = Order { start_time: 0,  duration: 9, bid: 100 }; 
         let b = Order { start_time: 10, duration: 3, bid: 140 }; 
         assert!(b.compatible(a));
+    }
+    #[test]
+    fn an_order_should_not_be_compatible_when_starting_before_end_time() {
+        let a = Order { start_time: 0,  duration: 9, bid: 100 }; 
+        let b = Order { start_time: 4, duration: 3, bid: 140 }; 
+        assert!(!b.compatible(a));
+    }
+    #[test]
+    fn an_order_should_be_compatible_when_starting_at_end_time() {
+        let a = Order { start_time: 0,  duration: 9, bid: 100 }; 
+        let b = Order { start_time: 9, duration: 3, bid: 140 }; 
+        assert!(b.compatible(a));
+    }
+    #[test]
+    fn a_plan_with_zero_orders_is_worth_zero() {
+        let p = vec![];
+        assert_eq!(0, value(p));
     }
 }
