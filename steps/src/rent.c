df142b4a0ae272f29354138d92bcf54fc4b0b03a
ToF- <cthibaut@octo.com>
2019-08-14 15:38:31 +0200

    step 2: having two cases with one order each

--- steps/src/rent.c
+++ steps/src/rent.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
+#include <assert.h>
+
+#define MAXLINE 80 /* arbitrary length of input line */
+
+char Line[MAXLINE];
+
+int get_int() {
+    int result;
+    fgets(Line, MAXLINE, stdin);
+    sscanf(Line, "%d", &result);
+    return result;
+}
 
 int main() {
+    int max_cases = get_int();
+
+    for(int i=0; i<max_cases; i++) {
+        int start_time, duration, price;
+        int max_orders = get_int(); /* we don't use the # of orders for now */
+        assert(max_orders==1);
+        fgets(Line, MAXLINE, stdin);
+        sscanf(Line, "%d %d %d", &start_time, &duration, &price);
+        printf("%d\n", price);
+    }   
     return 0;
 }
