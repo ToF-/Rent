a2b0d43e3eb8f6fc2c34e6727a0079f639da59b1
Christophe Thibaut <cthibaut@octo.com>
2019-08-15 08:54:17 +0200

    refactoring: extract method

--- Tutorial/step/rent.c
+++ Tutorial/step/rent.c
@@ -3,24 +3,39 @@
 
 char Line[MAXLINE];
 
-int main() {
-    int max_cases;
+struct order {
+    int start_time;
+    int duration;
+    int value;
+};
+
+int get_int() {
+    int result;
     fgets(Line, MAXLINE, stdin);
-    sscanf(Line, "%d", &max_cases);
+    sscanf(Line, "%d", &result);
+    return result;
+}
+
+void get_order(struct order *order) {
+
+    fgets(Line, MAXLINE, stdin);
+    sscanf(Line, "%d %d %d", 
+            &order->start_time, 
+            &order->duration, 
+            &order->value);
+}
+
+int main() {
+    int max_cases = get_int();
 
     for(int i=0; i<max_cases; i++) {
-        /* read the number of orders and ignore it */
-        fgets(Line, MAXLINE, stdin); 
-        int max_orders;
-        sscanf(Line, "%d", &max_orders);
+        int max_orders = get_int();
 
         int total = 0;
         for(int j=0; j<max_orders; j++) {
-            int start_time, duration, value;
-
-            fgets(Line, MAXLINE, stdin);
-            sscanf(Line, "%d %d %d", &start_time, &duration, &value);
-            total += value;
+            struct order order;
+            get_order(&order);
+            total += order.value;
         }
         printf("%d\n", total);
     }
