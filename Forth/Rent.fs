1810ab462d67e40097b5f5d801f15149a222ef80
ToF- <cthibaut@octo.com>
2020-07-21 10:33:01 +0200

    passes the test

--- Forth/Rent.fs
+++ Forth/Rent.fs
@@ -39,6 +39,12 @@ DECIMAL
         CELL+ SWAP CELL+ CELL+ SWAP
     LOOP 2DROP ;
 
+: POSITION@ ( a -- sd )
+    @ ORDER@ ORDER-POSITION ;
+
+: PRICE@ ( a -- p )
+    @ ORDER@ PRICE ;
+
 : MID ( l,h -- m )
     OVER - 2/ ALIGNED + ;
 
@@ -51,13 +57,13 @@ DECIMAL
     8 - ;
 
 : PARTITION ( l,h -- l',h',h'',l'' )
-    2DUP MID @ ORDER@ ORDER-POSITION >R
+    2DUP MID POSITION@ >R
     2DUP BEGIN
         SWAP BEGIN                       \ l,h,h,l 
-            DUP @ ORDER@ ORDER-POSITION R@ < WHILE CELL+ 
+            DUP POSITION@ R@ < WHILE CELL+ 
         REPEAT \ l,h,h,l'  | [l..l'[ < P
         SWAP BEGIN                       \ l,h,l',h
-            R@ OVER @ ORDER@ ORDER-POSITION < WHILE CELL- 
+            R@ OVER POSITION@ < WHILE CELL- 
         REPEAT
         2DUP <= IF 2DUP EXCH >R CELL+ R> CELL- THEN
         2DUP > UNTIL
@@ -74,7 +80,7 @@ DECIMAL
 
 : NEXT-COMPATIBLE ( p,sd,pa -- pa' )
     -ROT ADD-DURATION ORDER-POSITION >R
-    BEGIN DUP @ ORDER@ ORDER-POSITION R@ < WHILE CELL+ REPEAT
+    BEGIN DUP POSITION@ R@ < WHILE CELL+ REPEAT
     R> DROP ;
 
 : PRINT-ORDER ( p,sd -- )
@@ -82,5 +88,56 @@ DECIMAL
     2DUP DURATION   10 .R
     PRICE           10 .R ;
 
+: ADD-SENTINEL ( a,l -- )
+    2* CELLS + >R
+    2000000 0 0 MAKE-ORDER R> ORDER! ;
+
+: ORDERS-VALUE ( a,l -- n )
+    1- CELLS OVER + CELL-   \ a0,ai
+    2DUP BEGIN              \ a0,ai,a0,ai
+        >R
+        R@ @ ORDER@ R@ CELL+ NEXT-COMPATIBLE
+        PRICE@ R@ PRICE@ +
+        R@ CELL+ PRICE@ MAX
+        R@ @ ORDER@ NIP R@ @ ORDER! 
+        R> CELL-
+    2DUP > UNTIL 
+    2DROP DROP
+    PRICE@ ;
+
 : PRINT-ORDER-ARRAY ( a,l )
     0 DO DUP I CELLS + @ ORDER@ PRINT-ORDER CR LOOP DROP ;
+
+CREATE INPUT-LINE 40 ALLOT
+
+: READ-NUMBERS
+    INPUT-LINE DUP 40 STDIN READ-LINE THROW 
+    IF EVALUATE ELSE 2DROP THEN ;    
+
+VARIABLE MAX-ORDER
+CREATE ORDERS 10001 2* CELLS ALLOT
+CREATE POINTERS 10001 CELLS ALLOT
+
+: GET-ORDERS ( -- )
+    READ-NUMBERS DUP MAX-ORDER !
+    0 DO
+        READ-NUMBERS MAKE-ORDER
+        ORDERS I 2* CELLS + ORDER!
+    LOOP ;
+
+: PROCESS-ORDERS ( -- n )
+    ORDERS MAX-ORDER @ ADD-SENTINEL
+    1 MAX-ORDER +!
+    ORDERS POINTERS MAX-ORDER @ ORDER@-ARRAY
+    POINTERS MAX-ORDER @ SORT
+    POINTERS MAX-ORDER @ ORDERS-VALUE ;
+
+: PROCESS 
+    READ-NUMBERS 0 DO
+        GET-ORDERS
+        PROCESS-ORDERS
+        . CR
+    LOOP ;
+
+PROCESS
+BYE
